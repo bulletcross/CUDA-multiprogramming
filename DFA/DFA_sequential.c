9391a10c570f762ca0f65f9fc814e2f6ee5ccf00
bulletcross <bulletcross@gmail.com>
2015-04-08 17:21:03 +0530

    DFA sequential added

---
+++ DFA/DFA_sequential.c
@@ -0,0 +1,64 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int run_automata(int **t_m,int *in,int n_state,int n_sigma,int init_state,int final_state,int n){
+	int i;
+	int state = init_state;
+	for(i=0;i<n;i++){
+		printf("%d ",state);
+		state = t_m[state][in[i]];
+	}
+	if(state==final_state){
+		return 1;
+	}
+	else{
+		return 0;
+	}
+}
+
+int main(){
+	//Variables
+	int STATES,SIGMA,INITIAL_STATE,FINAL_STATE,INPUT_LENGTH;
+	int i,j;
+	//Taking input
+	//cin >> STATES >> SIGMA >> FINAL_STATE >> INPUT_LENGTH;
+	scanf("%d %d %d %d",&STATES,&SIGMA,&FINAL_STATE,&INPUT_LENGTH);
+	//An additional state has to be added for complete transition function
+	STATES++;
+	//Input memory allocation and input retrival
+	int *input = (int *)malloc(sizeof(int)*INPUT_LENGTH);
+	for(i=0;i<INPUT_LENGTH;i++){
+		scanf("%d",&input[i]);
+	}
+	//Allocating memory and retriving to transition matrix
+	int **transition_matrix = (int **)malloc(sizeof(int *)*STATES);
+	for(i=0;i<STATES;i++){
+		transition_matrix[i] = (int *)malloc(sizeof(int)*SIGMA);
+	}
+	for(i=0;i<STATES;i++){
+		for(j=0;j<SIGMA;j++){
+			scanf("%d",&transition_matrix[i][j]);
+		}
+	}
+	//printing the input taken
+	for(i=0;i<INPUT_LENGTH;i++){
+		printf("%d ",input[i]);
+	}
+	printf("\n");
+	for(i=0;i<STATES;i++){
+		for(j=0;j<SIGMA;j++){
+			printf("%d ",transition_matrix[i][j]);
+		}
+		printf("\n");
+	}
+	printf("\n");
+	//////////////////////////////////////////////////////////////////////////
+	if(run_automata(transition_matrix,input,STATES,SIGMA,INITIAL_STATE,FINAL_STATE,INPUT_LENGTH)){
+		printf("String is accepted by Automata\n");
+	}
+	else{
+		printf("String is not accepted ny Automata\n");
+	}
+
+	return 0;
+}
---
+++ test code/testcode.cu
@@ -0,0 +1,45 @@
+#include <stdio.h>
+ #include <stdlib.h>
+
+const int N = 16; 
+const int blocksize = 20; 
+
+//Kernel
+__global__  void block(int *d_ar,int size) {
+	//Local memory
+	int t_id = threadIdx.x;
+	if(t_id>=size){
+		return;
+	}
+	printf("Thread %d got %d\n",t_id,d_ar[t_id]);
+}
+
+void create_data(int *h_ar){
+	for(int i=0;i<N;i++){
+		h_ar[i] = (rand()%10) + 1;
+	}
+}
+ 
+int main()
+{
+	int size = N*sizeof(int);
+	//Host memory
+	int *h_ar  = (int *)malloc(size);
+ 	create_data(h_ar);
+ 	//Device memory
+	int *d_ar;
+ 	
+ 	//Allocating and initializing memory on GPU
+	cudaMalloc( (void**)&d_ar,size);
+	cudaMemcpy( d_ar, h_ar, size, cudaMemcpyHostToDevice );
+	
+	//Declaring grid and block size
+	dim3 dimBlock( blocksize, 1 ,1);
+	dim3 dimGrid( 1, 1 ,1);
+	block<<<dimGrid, dimBlock>>>(d_ar,N);
+
+	cudaFree( d_ar );
+	
+	printf("All done\n");
+	return 0;
+}
