48dc1c1ee2191f27d86958d0fc9797dfd20462fd
bulletcross <bulletcross@gmail.com>
2015-04-09 01:28:02 +0530

    DFA effecient reduction added

--- DFA/DFA_parallel_cuda_optimized.cu
+++ DFA/DFA_parallel_cuda_optimized.cu
@@ -1,19 +1,42 @@
+
 #include <stdio.h>
 #include <stdlib.h>
 
-//Kernel
-__global__  void DFA_kernal(int *t_m,int *in,int n_state,int n_sigma,int init_state,int final_state,int n,int *out) {
+//Kernel function - Given transition matrix and a part of input string,  thread creates an array of possible states
+//Reduction of state vector is done with O(P) - order of number of threads running
+__global__  void DFA_kernal(int *t_m,int *in,int n_state,int n_sigma,int init_state,int final_state,int n,int *out,int m,int n_t) {
+
+	//Shared arrays of state vector for faster retrival and writing, it is vector of state vectors for all threads.
 	extern __shared__ int state_vectors[];
-	int i,j;
+
+	//Variable used
+	int i,j,k;
+	//Thread ID assuming 1D block structure in 1D Grid structure
 	int t_id = threadIdx.x;
-	//int *state_vector = (int *)malloc(sizeof(int)*n_state);
-	//TO-DO: give this thread a part of string
+
+	//Initial retrival of possible state corresponding to first input
 	for(i=0;i<n_state;i++){
-		//state_vector[i] = t_m[n_sigma*i + in[t_id]];
-		state_vectors[n_state*t_id + i] = t_m[n_sigma*i + in[t_id]];
+		state_vectors[n_state*t_id + i] = t_m[n_sigma*i + in[t_id*m]];
 	}
+	// This thread will act on [i,j] part on input of length n
+	i=t_id*m+1;
+	j=(t_id+1)*m -1;
+	//Checks for boundary condition
+	if(j>=n){
+		j=n-1;
+	}
+
+	//Reuse the state vector alloted to the thread to compute final state vector
+	while(i<=j){
+		for(k=0;k<n_state;k++){
+			state_vectors[n_state*t_id + k] = t_m[n_sigma*state_vectors[n_state*t_id + k] + in[i]];
+		}
+		i++;
+	}
+	//Wait till all vector finishes
 	__syncthreads();
-	//O(P) reduction
+
+	//O(n_t) reduction. n_t is total number of threads in use in the given block
 	for(i = 1; i < blockDim.x; ++i) {
         __syncthreads();
         if(t_id == i) {
@@ -22,40 +45,51 @@ __global__  void DFA_kernal(int *t_m,int *in,int n_state,int n_sigma,int init_st
             }
         }
     }
-    for(int i=0;i<n_state;i++){
-    	out[i] = state_vectors[n_state*(n-1) + i];
-    }
 
+    //Final output is copied to global device memory
+    if(t_id==0){
+    	for(i=0;i<n_state;i++){
+    		out[i] = state_vectors[n_state*(n_t-1) + i];
+    	}
+    }
 }
  
+ /*******************************************Main Function starts here*******************************************/
+
 int main()
 {
-	//Variables
+	//Variables requred for transition function and input memory allocation
 	int STATES,SIGMA,INITIAL_STATE,FINAL_STATE,INPUT_LENGTH;
 	int i,j;
+
 	//Taking input
-	//cin >> STATES >> SIGMA >> FINAL_STATE >> INPUT_LENGTH;
 	scanf("%d %d %d %d",&STATES,&SIGMA,&FINAL_STATE,&INPUT_LENGTH);
+	
+	//Number of threads depend on optimality condition
+	int NUM_THREADS=4;
+	int ELEM_PER_THREAD= ((INPUT_LENGTH%NUM_THREADS==0) ? INPUT_LENGTH/NUM_THREADS:INPUT_LENGTH/NUM_THREADS + 1);
 	//An additional state has to be added for complete transition function
 	STATES++;
 	INITIAL_STATE = 0;
+	
 	//Input memory allocation and input retrival
 	int *input = (int *)malloc(sizeof(int)*INPUT_LENGTH);
 	for(i=0;i<INPUT_LENGTH;i++){
 		scanf("%d",&input[i]);
 	}
+	
 	//Allocating memory and retriving to transition matrix
 	int **transition_matrix = (int **)malloc(sizeof(int *)*STATES);
 	int *transition_matrix_data = (int *)malloc(sizeof(int)*STATES*SIGMA);
 	for(i=0;i<STATES;i++){
 		transition_matrix[i] = &transition_matrix_data[i*SIGMA];
 	}
-
 	for(i=0;i<STATES;i++){
 		for(j=0;j<SIGMA;j++){
 			scanf("%d",&transition_matrix[i][j]);
 		}
 	}
+	
 	//printing the input taken
 	for(i=0;i<INPUT_LENGTH;i++){
 		printf("%d ",input[i]);
@@ -68,8 +102,10 @@ int main()
 		printf("\n");
 	}
 	printf("\n");
+
+	//final output memory requred by device 
 	int *h_out = (int *)malloc(sizeof(int)*STATES);
-	//////////////////////////////////////////////////////////////////////////
+
  	//Device memory
 	int *d_transition_matrix;
 	int *d_input;
@@ -85,19 +121,26 @@ int main()
 	cudaMalloc((void**)&d_output,sizeof(int)*STATES);
 	
 	//Declaring grid and block size
-	dim3 dimBlock(INPUT_LENGTH,1,1);
+	dim3 dimBlock(NUM_THREADS,1,1);
 	dim3 dimGrid(1,1,1);
-	DFA_kernal<<<dimGrid, dimBlock, STATES*INPUT_LENGTH>>>(d_transition_matrix,d_input,STATES,SIGMA,INITIAL_STATE,FINAL_STATE,INPUT_LENGTH,d_output);
 
+	//Calling Kernal funtion to execute in on device
+	DFA_kernal<<<dimGrid, dimBlock, STATES*NUM_THREADS>>>(d_transition_matrix,d_input,STATES,SIGMA,INITIAL_STATE,FINAL_STATE,INPUT_LENGTH,d_output,ELEM_PER_THREAD,NUM_THREADS);
+
+	//Copy back the computed result from device to host
 	cudaMemcpy((void *)h_out,(void *)d_output,sizeof(int)*STATES,cudaMemcpyDeviceToHost);
 
+	//Freeing-up the GPU intialized memory
 	cudaFree(d_output);
 	cudaFree(d_transition_matrix);
 	cudaFree(d_input);
 
-	/*for(i=0;i<STATES;i++){
+	//Print the compueted output
+	for(i=0;i<STATES;i++){
 		printf("%d\n",h_out[i]);
-	}*/
+	}
+
+	//Condition checking
 	if(h_out[0]==FINAL_STATE){
 		printf("Automata is accepting the string\n");
 	}
@@ -106,5 +149,8 @@ int main()
 	}
 
 	printf("All done\n");
+
 	return 0;
 }
+
+/***********************************************Main ends here**********************************************************/
